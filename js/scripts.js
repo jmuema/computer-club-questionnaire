fbab8e23d109ba4ceb75c51eae8da8a93da31196
josephmuema <jmuema95@gmail.com>
2019-03-31 18:07:00 +0300

    Add another statement

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+function quiz(){
+
+  var score = 0;
+  var newScore = 0;
+  var firstQuestion = document.forms["myForm"]["firstQuestion"].value;
+  var secondQuestion = document.forms["myForm"]["secondQuestion"].value;
+  var thirdQuestion = document.forms["myForm"]["thirdQuestion"].value;
+  var fourthQuestion = document.forms["myForm"]["fourthQuestion"].value;
+  var fifthQuestion = document.forms["myForm"]["fifthQuestion"].value;
+
+  for(i=0; i<=5; i++){
+    if(firstQuestion == "4"){
+      newScore = score+1;
+    }
+    if(secondQuestion == "4"){
+      newScore=newScore+1;
+    }
+    if(thirdQuestion == "4") {
+      newScore=newScore+1;
+    }
+    if (fourthQuestion == "4"){
+      newScore=newScore+1;
+    }
+    if (fifthQuestion == "1"){
+      newScore=newScore+1;
+    }
+
+  }
+
+    var percentageScore = (newScore/5) * 100;
+
+  document.getElementById("message").innerHTML="You scored " + percentageScore + "%";
+  document.getElementById("result").style.visibility = "visible";
+  e.preventDefault();
+}
